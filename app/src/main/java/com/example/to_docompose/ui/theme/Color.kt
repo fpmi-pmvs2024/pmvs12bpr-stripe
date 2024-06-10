a33f2d7fb0c808f63869e057fadde262318159df
DzimaSh <dzmitry.shushkevich@gmail.com>
2024-06-10 15:45:32 +0300

    fix layout

---
+++ app/src/main/java/com/example/to_docompose/ui/theme/Color.kt
@@ -0,0 +1,43 @@
+package com.example.to_docompose.ui.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFF000000)
+val Purple500 = Color(0x29000000)
+val Purple700 = Color(0xFF000000)
+val Teal200 = Color(0xFF03DAC5)
+
+val LightGray = Color(0xFFFCFCFC)
+val MediumGray = Color(0xFF9C9C9C)
+val DarkGray = Color(0xFF141414)
+
+val LowPriorityColor = Color(0xFF00C980)
+val MediumPriorityColor = Color(0xFFFFC114)
+val HighPriorityColor = Color(0XFFFF4646)
+val NonePriorityColor = MediumGray
+
+val Colors.fabBackgroundColor: Color
+@Composable
+get() = if (isLight) Color.Black else MediumGray
+
+val Colors.topAppBarContentColor: Color
+@Composable
+get() = if (isLight) Color.Black else Color.White
+
+val Colors.topAppBarBackgroundColor: Color
+@Composable
+get() = if (isLight) MediumGray else DarkGray
+
+val Colors.taskItemBackgroundColor: Color
+@Composable
+get() = if (isLight) LightGray else DarkGray
+
+val Colors.taskItemTextColor: Color
+@Composable
+get() = if (isLight) DarkGray else LightGray
+
+val Colors.splashScreenBackground: Color
+@Composable
+get() = if (isLight) DarkGray else LightGray
---
+++ app/src/main/java/com/example/to_docompose/ui/viewmodels/SharedViewModel.kt
@@ -0,0 +1,209 @@
+package com.example.to_docompose.ui.viewmodels
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.to_docompose.data.models.Priority
+import com.example.to_docompose.data.models.ToDoTask
+import com.example.to_docompose.data.repositories.DataStoreRepository
+import com.example.to_docompose.data.repositories.ToDoRepository
+import com.example.to_docompose.util.Action
+import com.example.to_docompose.util.Constants.MAX_TITLE_LENGTH
+import com.example.to_docompose.util.RequestState
+import com.example.to_docompose.util.SearchAppBarState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SharedViewModel @Inject constructor(
+    private val toDoRepository: ToDoRepository,
+    private val dataStoreRepository: DataStoreRepository,
+) : ViewModel() {
+
+    val action: MutableState<Action> = mutableStateOf(Action.NO_ACTION )
+
+    val id: MutableState<Int> = mutableStateOf(0)
+    val title: MutableState<String> = mutableStateOf("")
+    val description: MutableState<String> = mutableStateOf("")
+    val priority: MutableState<Priority> = mutableStateOf(Priority.LOW)
+
+    val searchAppBarState: MutableState<SearchAppBarState> =
+        mutableStateOf(SearchAppBarState.CLOSED)
+    val searchTextState: MutableState<String> = mutableStateOf("")
+
+    private val _searchedTasks = MutableStateFlow<RequestState<List<ToDoTask>>>(RequestState.Idle)
+    val searchedTasks: StateFlow<RequestState<List<ToDoTask>>> = _searchedTasks
+
+    val lowPriorityTasks: StateFlow<List<ToDoTask>> = toDoRepository.sortByLowPriority.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        emptyList()
+    )
+
+    val highPriorityTasks: StateFlow<List<ToDoTask>> = toDoRepository.sortByHighPriority.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        emptyList()
+    )
+
+    private val _sortState = MutableStateFlow<RequestState<Priority>>(RequestState.Idle)
+    val sortState: StateFlow<RequestState<Priority>> = _sortState
+
+    fun readSortState() {
+        _sortState.value = RequestState.Loading
+        try {
+            viewModelScope.launch {
+                dataStoreRepository.readSortState
+                    .map { Priority.valueOf(it) }
+                    .collect {
+                        _sortState.value = RequestState.Success(it)
+                    }
+            }
+        } catch (e: Exception) {
+            _sortState.value = RequestState.Error(e)
+        }
+    }
+
+    fun persistSortState(priority: Priority) {
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStoreRepository.persistSortState(priority)
+        }
+    }
+
+    private val _allTasks = MutableStateFlow<RequestState<List<ToDoTask>>>(RequestState.Idle)
+    val allTasks: StateFlow<RequestState<List<ToDoTask>>> = _allTasks
+
+    private val _selectedTask: MutableStateFlow<ToDoTask?> = MutableStateFlow(null)
+    val selectedTask: StateFlow<ToDoTask?> = _selectedTask
+
+    fun searchDatabase(searchQuery: String) {
+        _searchedTasks.value = RequestState.Loading
+        try {
+            viewModelScope.launch {
+                toDoRepository.searchDatabase("%$searchQuery%")
+                    .collect{ searchedTasks ->
+                        _searchedTasks.value = RequestState.Success(searchedTasks)
+                    }
+            }
+        } catch (e: Exception) {
+            _searchedTasks.value = RequestState.Error(e)
+        }
+        searchAppBarState.value = SearchAppBarState.TRIGGERED
+    }
+
+    fun getAllTasks() {
+        _allTasks.value = RequestState.Loading
+        try {
+            viewModelScope.launch {
+                toDoRepository.getAllTasks.collect { tasks ->
+                    _allTasks.value = RequestState.Success(tasks)
+                }
+            }
+        } catch (e: Exception) {
+            _allTasks.value = RequestState.Error(e)
+        }
+    }
+
+    fun getSelectedTasks(taskId: Int) {
+        viewModelScope.launch {
+            toDoRepository.getSelectedTask(taskId).collect { task ->
+                _selectedTask.value = task
+            }
+        }
+    }
+
+    private fun addTask() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val toDoTask = ToDoTask(
+                title = title.value,
+                description = description.value,
+                priority = priority.value
+            )
+            toDoRepository.addTask(toDoTask)
+        }
+        searchAppBarState.value = SearchAppBarState.CLOSED
+    }
+
+    private fun updateTask() {
+         viewModelScope.launch(Dispatchers.IO) {
+             val toDoTask = ToDoTask(
+                 id = id.value,
+                 title = title.value,
+                 description = description.value,
+                 priority = priority.value
+             )
+             toDoRepository.updateTask(toDoTask)
+         }
+    }
+
+    private fun deleteTask() {
+         viewModelScope.launch(Dispatchers.IO) {
+             val toDoTask = ToDoTask(
+                 id = id.value,
+                 title = title.value,
+                 description = description.value,
+                 priority = priority.value
+             )
+             toDoRepository.deleteTask(toDoTask)
+         }
+    }
+
+
+    private fun deleteAllTasks() {
+        viewModelScope.launch(Dispatchers.IO) {
+            toDoRepository.deleteAllTasks()
+        }
+    }
+
+    fun handleDatabaseActions(action: Action) {
+        when (action) {
+            Action.ADD -> {
+                addTask()
+            }
+            Action.UPDATE -> {
+                updateTask()
+            }
+            Action.DELETE -> {
+                deleteTask()
+            }
+            Action.DELETE_ALL -> {
+                deleteAllTasks()
+            }
+            Action.UNDO -> {
+                addTask()
+            }
+            else ->  {
+
+            }
+        }
+        this.action.value = Action.NO_ACTION
+    }
+
+    fun updateTaskFields(selectedTask: ToDoTask?) {
+        if (selectedTask != null) {
+            id.value = selectedTask.id
+            title.value = selectedTask.title
+            description.value = selectedTask.description
+            priority.value = selectedTask.priority
+        } else {
+            id.value = 0
+            title.value = ""
+            description.value = ""
+            priority.value = Priority.LOW
+        }
+    }
+
+    fun updateTitle(newTitle: String) {
+        if (newTitle.length < MAX_TITLE_LENGTH) {
+            title.value = newTitle
+        }
+    }
+
+    fun validateFields(): Boolean {
+        return title.value.isNotEmpty() && description.value.isNotEmpty()
+    }
+}
